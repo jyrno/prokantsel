725e21c27bd279c2876bc320e10d933ce8527bab
Taavi Gilden <u333410@danfoss.com>
2019-11-20 20:56:18 +0200

    Minor changes

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -14,6 +14,7 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
+    console.log("testsetestse");
     this.setState({
       listItems: [
         {
@@ -43,6 +44,7 @@ export default class App extends React.Component {
 
       // change the paragraph color to blue.
       paragraph.font.color = "blue";
+      context.document.body.select();
 
       await context.sync();
     });
@@ -50,18 +52,49 @@ export default class App extends React.Component {
 
   highlight = async () => {
     return Word.run(async context => {
-      /**
-       * Insert your Word code here
-       */
+
+      var body = context.document.body;
+      const myParagraphs = body.paragraphs;
+      myParagraphs.load("text");
+      await context.sync();
+
+      myParagraphs.items.forEach(element => {
+        console.log(element.text);
+        element.font.color = "red";
+      });
+      // context.load(body, 'text');
 
       // insert a paragraph at the end of the document.
-      const paragraph = context.document.body.insertParagraph("Hellooo World", Word.InsertLocation.end);
-
-      // change the paragraph color to blue.
+      const paragraph = context.document.body.insertParagraph("JOUOE", Word.InsertLocation.end);
       paragraph.font.color = "blue";
-      paragraph.font.highlightColor = "yellow"
+      //console.log(paragraph);
+
+
+      // await context.sync();
+      //console.log("tes3322");
+
+      let paragraphs = context.document.body.paragraphs;
+      paragraphs.load("text");
+      //console.log("tes3322");
 
       await context.sync();
+    
+      let text = [];
+      paragraphs.items.forEach((item) => {
+        let paragraph = item.text.trim();
+        console.log(item.text);
+        if (paragraph) {
+          paragraph.split(" ").forEach((term) => {
+            let currentTerm = term.trim();
+            if (currentTerm) {
+              currentTerm.toUpperCase();
+              text.push(currentTerm);
+            }
+          });
+        }
+      });
+      //console.log("test222");
+      console.log(text);
     });
   };
 
