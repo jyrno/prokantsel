82a8ee12ce1c2cddae623b41631b070128315ba9
Triinu Liis Kelder <triinuk@proekspert.ee>
2019-11-22 13:42:29 +0200

    Stuff

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -232,6 +232,10 @@ export default class App extends React.Component {
       // documentBody.font.color = 'black';
       await context.sync();
 
+      this.setState({
+        bulpitWords: []
+      });
+
     })
     .catch(function (error) {
       console.log('Error: ' + JSON.stringify(error));
@@ -282,20 +286,20 @@ export default class App extends React.Component {
           {this.state.complexity && (
             <p className="bulpit__complexity">
               {this.state.complexity.coef > 75 ? (
-                <Fragment>
+                <div className="bulpit__complexity bulpit__complexity--bad">
                   <svg className="bulpit__complexity-img bulpit__complexity-img--bad" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.001 14c-2.332 0-4.145 1.636-5.093 2.797l.471.58c1.286-.819 2.732-1.308 4.622-1.308s3.336.489 4.622 1.308l.471-.58c-.948-1.161-2.761-2.797-5.093-2.797zm-3.501-6c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5-.672-1.5-1.5-1.5zm7 0c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5-.672-1.5-1.5-1.5z"/></svg>
                   {this.state.complexity.text}
-                </Fragment>
+                </div>
               ) : this.state.complexity.coef > 50 ? (
-                <Fragment>
+                <div className="bulpit__complexity bulpit__complexity--ok">
                   <svg className="bulpit__complexity-img bulpit__complexity-img--ok" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4 17h-8v-2h8v2zm-7.5-9c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5-.672-1.5-1.5-1.5zm7 0c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5-.672-1.5-1.5-1.5z"/></svg>
                   {this.state.complexity.text}
-                </Fragment>
+                </div>
               ) : (
-                <Fragment>
+                <div className="bulpit__complexity bulpit__complexity--good">
                   <svg className="bulpit__complexity-img bulpit__complexity-img--good" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 14h-12c.331 1.465 2.827 4 6.001 4 3.134 0 5.666-2.521 5.999-4zm0-3.998l-.755.506s-.503-.948-1.746-.948c-1.207 0-1.745.948-1.745.948l-.754-.506c.281-.748 1.205-2.002 2.499-2.002 1.295 0 2.218 1.254 2.501 2.002zm-7 0l-.755.506s-.503-.948-1.746-.948c-1.207 0-1.745.948-1.745.948l-.754-.506c.281-.748 1.205-2.002 2.499-2.002 1.295 0 2.218 1.254 2.501 2.002z"/></svg>
                   {this.state.complexity.text}
-                </Fragment>
+                </div>
               )}
             </p>
           )}
@@ -311,6 +315,7 @@ export default class App extends React.Component {
               onReplace={this.replaceSinglePhrase}
             />
           ))}
+          {this.state.bulpitWords.length > 0 && <p className="bulpit__ignore-all" onClick={this.cleanDocument}>Ignoreeri kõik</p>}
         </main>
       </div>
     );
